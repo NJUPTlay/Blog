34e5534cf03a9cbc2373617c7d8fd6421cd85f37
lay <2782522198@qq.com>
2024-12-16 19:55:31 +0800

    feat: eslint-noconsole demo

---
+++ docs/src/AST/ast6.mjs
@@ -0,0 +1,48 @@
+import core from '@babel/core';
+import types from '@babel/types';
+
+const sourceCode = `
+var a = 1;
+console.log(a);
+var b = 2;
+`;
+
+
+
+//no-console 禁用 console fix=true 自动修复
+const eslintPlugin=({fix})=>{
+    return {
+        //AST遍历也会有自己的生命周期,有两个钩子,在遍历之前和之后 pre和post
+        pre(file){
+            file.set('errors',[]);
+        },
+        visitor:{
+            CallExpression(path,state){
+                const errors = state.file.get("errors");
+                const {node}=path;
+                if(types.isMemberExpression(node.callee)){
+                    if(node.callee.object.name==='console'){
+                        errors.push(
+                            path.buildCodeFrameError(`代码中不能出现console语句`, Error)
+                        );
+                        if(fix){
+                            //启动了自动修复就删除console语句
+                            path.parentPath.remove();
+                        }
+                    }
+                }
+            }
+        },
+        post(file){
+            console.log(...file.get('errors'));
+            
+        },
+    }
+}
+
+let targetSource = core.transform(sourceCode, {
+    plugins: [eslintPlugin({ fix: true })], //使用插件
+  });
+  
+console.log(targetSource.code);
+  
